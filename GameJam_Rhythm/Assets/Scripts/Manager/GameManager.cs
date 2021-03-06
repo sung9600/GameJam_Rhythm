5f1c2d90c5d4aad41138c2dcd86f9c16f2e74116
sung9600 <sung96002@naver.com>
2021-03-06 17:39:47 +0900

    GameJam_Init

---
+++ GameJam_Rhythm/Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<float> _perfectTimes;
+    public static GameManager instance = null;
+    public static GameManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+    void Start()
+    {
+        if(instance==null){
+            instance= this;
+        }
+        StreamReader sr=new StreamReader($"{Application.dataPath}/Resource/csv/Notes.csv");
+        bool _isEOF=false;
+        while(!_isEOF){
+            string data=sr.ReadLine();
+            if(data==null){
+                _isEOF=true;
+                break;
+            }
+            var data_values = data.Split(',');
+            _perfectTimes.Add(float.Parse(data_values[0]));
+        }
+        sr.Close();
+        foreach(float a in _perfectTimes){
+            Debug.Log(a);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GameJam_Rhythm/Assets/Scripts/Manager/InputManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            float _diffTime = Mathf.Abs(GameManager.Instance._perfectTimes[0] - Time.time);
+            if (_diffTime < 0.5f)
+            {
+                Debug.Log("Perfect");
+
+            }
+            else
+            {
+                Debug.Log("Bad");
+            }
+            GameManager.Instance._perfectTimes.RemoveAt(0);
+        }
+    }
+}
